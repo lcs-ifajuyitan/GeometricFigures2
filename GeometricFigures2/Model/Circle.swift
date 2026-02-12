cdf6929c3bf75b881526c98cd560d0b7e9ea3e66
IBK Fajuyitan <ifajuyitan@lcs.on.ca>
2026-02-12 10:42:50 -0500

    Created groups for model and view layers of the app.

--- GeometricFigures2/Model/Circle.swift
+++ GeometricFigures2/Model/Circle.swift
@@ -1,8 +1,33 @@
-//
-//  Circle.swift
-//  GeometricFigures2
-//
-//  Created by ☆ on 12/02/2026.
-//
-
-import Foundation
+// Define the protocol
+protocol Describable {
+    var description: String { get }
+}
+ 
+// Define the structure, adopting the new protocol
+struct Circle: Describable {
+    
+    // MARK: Stored properties
+    var radius: Double
+    
+    // MARK: Computed properties
+    var diameter: Double {
+        return radius * 2
+    }
+    
+    var area: Double {
+        return Double.pi * radius * radius
+    }
+    
+    var perimeter: Double {
+        return 2 * Double.pi * radius
+    }
+    
+    var circumference: Double {
+        return perimeter
+    }
+    
+    var description: String {
+        return "The radius of this circle is \(radius) units."
+    }
+    
+}
