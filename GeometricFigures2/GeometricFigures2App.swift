3021054790a2a62431c4e32f56ab2f0c0773efcf
IBK Fajuyitan <ifajuyitan@lcs.on.ca>
2026-02-12 09:59:35 -0500

    Initial Commit

---
+++ GeometricFigures2/GeometricFigures2App.swift
@@ -0,0 +1,17 @@
+//
+//  GeometricFigures2App.swift
+//  GeometricFigures2
+//
+//  Created by ☆ on 12/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct GeometricFigures2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
